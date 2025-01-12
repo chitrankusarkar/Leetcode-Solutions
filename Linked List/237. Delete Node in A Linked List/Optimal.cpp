c5f950c3cede8eb247f0a4edc1d35aa7be07bcfe
chitrankusarkar <109690584+chitrankusarkar@users.noreply.github.com>
2025-01-13 02:49:29 +0530

    Create Optimal.cpp

---
+++ Linked List/237. Delete Node in A Linked List/Optimal.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    void deleteNode(ListNode* node) {
+        ListNode *next = node->next;
+        *node = *next;
+        /* 
+        node->val = next->val;
+        node->next = next->next;
+        */
+        delete next;
+    }
+};
