67b08849bb16ef216c5fae1775ba338d03113fea
chitrankusarkar <109690584+chitrankusarkar@users.noreply.github.com>
2025-02-04 19:12:12 +0530

    Create Solve2.cpp

---
+++ Arrays/1800. Maximum Ascending Subarray Sum/Solve2.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int maxAscendingSum(vector<int>& nums) {
+        int n = nums.size();
+        int sum = nums[0], maxSum = nums[0];
+        for(int i = 1; i < n; i++) {
+            if(nums[i] > nums[i - 1])
+                sum += nums[i];
+            else
+                sum = nums[i];
+            maxSum = max(sum, maxSum);
+        }
+        return maxSum;
+    }
+};
