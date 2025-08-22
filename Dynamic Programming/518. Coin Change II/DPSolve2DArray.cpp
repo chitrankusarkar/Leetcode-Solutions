8027d8d0aafc77c9ae8b9a4a3c62411d036edef5
Chitranku Sarkar <109690584+chitrankusarkar@users.noreply.github.com>
2025-08-23 03:48:51 +0530

    Update DPSolve2DArray.cpp

--- Dynamic Programming/518. Coin Change II/DPSolve2DArray.cpp
+++ Dynamic Programming/518. Coin Change II/DPSolve2DArray.cpp
@@ -2,9 +2,10 @@ class Solution {
 public:
     int change(int amount, vector<int>& coins) {
         int n = coins.size();
-        vector<vector<int>> dp(n + 1, vector<int>(amount + 1));
-        for(int i = 0; i <= n; i++) {
-            for(int j = 0; j <= amount; j++) {
+        long long dp[n + 1][amount + 1];
+        long long MOD = INT_MAX;
+        for(int i = 0; i < n + 1; i++) {
+            for(int j = 0; j < amount + 1; j++) {
                 if(i == 0)
                     dp[i][j] = 0;
                 if(j == 0)
@@ -15,12 +16,12 @@ public:
         for(int i = 1; i <= n; i++) {
             for(int j = 1; j <= amount; j++) {
                 if(coins[i - 1] <= j) {
-                    dp[i][j] = dp[i][j - coins[i - 1]] + dp[i - 1][j];
+                    dp[i][j] = dp[i][j - coins[i - 1]] + dp[i - 1][j] % MOD;
                 }
                 else
-                    dp[i][j] = dp[i - 1][j];
+                    dp[i][j] = dp[i - 1][j] % MOD;
             }
         }
-        return dp[n][amount];
+        return dp[n][amount] % MOD;
     }
 };
