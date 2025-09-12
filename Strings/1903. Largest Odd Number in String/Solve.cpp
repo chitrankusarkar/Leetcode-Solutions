ffc95e2ad50d205d6c207909354a706bfebc592a
Chitranku Sarkar <109690584+chitrankusarkar@users.noreply.github.com>
2025-09-12 20:44:42 +0530

    Create Solve.cpp

---
+++ Strings/1903. Largest Odd Number in String/Solve.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    string largestOddNumber(string num) {
+        int n = num.size();
+        if(num.back() % 2 != 0)
+            return num;
+        for(int i = n - 1; i >= 0; i--) {
+            int curr = num[i];
+            if(curr % 2 != 0) {
+                return num.substr(0, i + 1);
+            }
+        }
+        return "";
+    }
+};
