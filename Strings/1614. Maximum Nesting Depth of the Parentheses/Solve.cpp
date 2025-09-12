8ea0193cec03ac300604cbf3c718497f2e3f5dd2
Chitranku Sarkar <109690584+chitrankusarkar@users.noreply.github.com>
2025-09-12 20:43:46 +0530

    Create Solve.cpp

---
+++ Strings/1614. Maximum Nesting Depth of the Parentheses/Solve.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int maxDepth(string s) {
+        int cnt = 0, ans = 0;
+        for(int i = 0; i < s.size(); i++) {
+            if(s[i] == '(') {
+                cnt++;
+                ans = max(ans, cnt);
+            }
+            if(s[i] == ')')
+                cnt--;
+        }
+        return ans;
+    }
+};
