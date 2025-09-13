f154e59b2a94c23b51680e16f42f925ed03d633b
Chitranku Sarkar <109690584+chitrankusarkar@users.noreply.github.com>
2025-09-13 21:37:41 +0530

    Create Optimal.cpp

---
+++ Stack/Next Greater Element/Optimal.cpp
@@ -0,0 +1,24 @@
+class Solution {
+  public:
+    vector<int> nextLargerElement(vector<int>& arr) {
+        vector<int> res;
+        stack<int> s;
+        for(int i = arr.size() - 1; i >= 0; i--) {
+            if(s.empty())
+                res.push_back(-1);
+            else if(s.size() > 0 && arr[i] < s.top())
+                res.push_back(s.top());
+            else if(s.size() > 0 && arr[i] >= s.top()) {
+                while(s.size() > 0 && arr[i] >= s.top())
+                    s.pop();
+                if(s.empty())
+                    res.push_back(-1);
+                else
+                    res.push_back(s.top());
+            }
+            s.push(arr[i]);
+        }
+        reverse(res.begin(), res.end());
+        return res;
+    }
+};
