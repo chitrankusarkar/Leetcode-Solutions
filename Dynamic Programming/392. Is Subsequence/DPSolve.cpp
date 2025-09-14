474b05c15c23a75b83303baa2565cd48f91fef62
Chitranku Sarkar <109690584+chitrankusarkar@users.noreply.github.com>
2025-09-15 02:08:17 +0530

    Create DPSolve.cpp

---
+++ Dynamic Programming/392. Is Subsequence/DPSolve.cpp
@@ -0,0 +1,28 @@
+class Solution {
+    int helper(string s, string t, int m, int n) {
+        vector<vector<int>> dp(m + 1, vector<int>(n + 1));
+        for(int i = 0; i < m + 1; i++) {
+            for(int j = 0; j < n + 1; j++) {
+                if(i == 0 || j == 0)
+                    dp[i][j] = 0;
+            }
+        }
+        for(int i = 1; i <= m; i++) {
+            for(int j = 1; j <= n; j++) {
+                if(s[i - 1] == t[j - 1])
+                    dp[i][j] = 1 + dp[i - 1][j - 1];
+                else 
+                    dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
+            }
+        }
+        return dp[m][n];
+    }
+public:
+    bool isSubsequence(string s, string t) {
+        int m = s.size(), n = t.size();
+        int res = helper(s, t, m, n);
+        if(res == m) 
+            return true;
+        return false;
+    }
+};
